70d4b42d4570c4902979c1e9705fd88f60531eef
vocterr <kozanecki.bartosz@interia.pl>
2025-01-05 21:42:51 +0100

    xd

--- app/budgets/page.tsx
+++ app/budgets/page.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 
 export default async function BudgetsPage() {
     const cookieStore = await cookies();
+    console.log(cookieStore.getAll());
     const token = cookieStore.get("token")?.value;
     const budgets: Budget[] = await fetchBudgetsOnServer(token);
     return (
