236802ff0c48e25a720ac57a5b8c6673063c8317
vocterr <kozanecki.bartosz@interia.pl>
2025-01-06 00:05:29 +0100

    xd

--- next.config.ts
+++ next.config.ts
@@ -7,25 +7,23 @@ const nextConfig: NextConfig = {
         protocol: "http",
         hostname: "localhost",
         port: "3001",
-        pathname: "/**",
+        pathname: "/**", // Allows loading images from your backend
       },
     ],
   },
   async headers() {
     return [
-        {
-            source: "/(.*)",
-            headers: [
-                { key: "Access-Control-Allow-Credentials", value: "true" },
-                { key: "Access-Control-Allow-Origin", value: "https://portfolio1frontend.vercel.app" },
-                { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,POST,PUT,DELETE" },
-                { key: "Access-Control-Allow-Headers", value: "Content-Type,Authorization" },
-            ],
-        },
+      {
+        source: "/(.*)", // Applies these headers globally to all routes
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://portfolio1frontend.vercel.app" }, // Frontend origin
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,POST,PUT,DELETE" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type,Authorization" },
+        ],
+      },
     ];
-},
-
-  
+  },
 };
 
 export default nextConfig;
