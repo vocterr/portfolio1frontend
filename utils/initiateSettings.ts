e2b3ed479dcd5421e4d3731c90b95ed0325a07f5
vocterr <kozanecki.bartosz@interia.pl>
2025-01-06 17:13:49 +0100

    t

--- utils/initiateSettings.ts
+++ utils/initiateSettings.ts
@@ -1,7 +1,7 @@
 
 export const initiateSettings = async (token: any) => {
     try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/settings`, {
+        const res = await fetch(`/api/settings`, {
             method: "POST",
             headers: {
                 "Content-Type" : "application/json",
